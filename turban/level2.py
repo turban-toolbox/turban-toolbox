a73d2573b88d17642c4ef5d60fabdc085d49c74b
doppler <ara@akvaplan.niva.no>
2025-03-02 10:02:40 +0100

    replace shear spikes as python function

--- turban/level2.py
+++ turban/level2.py
@@ -112,12 +112,15 @@ def boolarr_to_sections(bools: Bool[ndarray, "time"]) -> List[List[int]]:
     return sections
 
 
-def sections_to_marker(sections: List[List[int]]) -> Int[ndarray, "time"]:
+def sections_to_marker(
+    sections: List[List[int]],
+    n: int,  # length of time series
+) -> Int[ndarray, "time"]:
     """Convert a list of sections to a marker array.
     0 means no section, 1 means first section, etc."""
-    marker = np.zeros(len(sections), dtype=int)
+    marker = np.zeros(n, dtype=int)
     for i, sec in enumerate(sections):
-        marker[sec] = i
+        marker[sec] = i + 1  # 0 is reserved for no section
     return marker
 
 
@@ -181,8 +184,8 @@ def clean_shear(
     int,  # number of despike iterations
 ]:
     """Section 3.2.2"""
+    N = len(sh)
     ctr = 0
-    sh_raw = sh.copy()
     spikes = detect_shear_spikes(
         sh,
         sampling_freq,
@@ -190,18 +193,20 @@ def clean_shear(
         spike_include_before=spike_include_before,
         spike_include_after=spike_include_after,
     )
+    ctr = np.zeros_like(sh, dtype=bool)
     while np.any(spikes) and (ctr < max_tries):
-        ctr += 1
+        sh_previous = sh.copy()
         spike_sections = boolarr_to_sections(spikes)
-
-        # sh = np.array(
-        #     replace_spikes(
-        #         sh,
-        #         spike_sections,
-        #         spike_replace_before=spike_replace_before,
-        #         spike_replace_after=spike_replace_after,
-        #     )
-        # )
+        spike_markers = sections_to_marker(spike_sections, N)
+        sh = np.array(
+            replace_spikes(
+                sh,
+                spike_markers,
+                spike_replace_before=spike_replace_before,
+                spike_replace_after=spike_replace_after,
+            )
+        )
+        ctr[sh != sh_previous] += 1
         warnings.warn("replace_spikes not implemented in python", UserWarning)
         spikes = detect_shear_spikes(
             sh,
@@ -218,12 +223,33 @@ def clean_shear(
 
 def split_data(
     data: Num[ndarray, "... time"],
-    data_and_bounds: data_and_bounds_type,
-) -> Tuple[
-    List[Num[ndarray, "... time"]],  # segments
-    List[List[int]],  # indices
-]:
-    """Dict of arrays to list of dict of arrays, split by `sections`"""
-    sections = select_sections(data_and_bounds)
-    segments = [data[..., inds] for inds in sections]
-    return segments, sections
+
+from numba import jit
+
+
+@jit
+def replace_spikes(sh, spike_markers, spike_replace_before, spike_replace_after):
+    """In-place replacement of shear spikes according to atomix procedure"""
+    for marker in np.unique(spike_markers):
+        if marker == 0:
+            continue
+        (spike,) = np.where(spike_markers == marker)
+        start = min(spike)
+        stop = max(spike)
+        replace_spike(sh, start, stop, spike_replace_before, spike_replace_after)
+
+
+@jit
+def replace_spike(
+    sh,  #: Float[ndarray, "time"],
+    start,
+    stop,
+    spike_replace_before,  #: int,
+    spike_replace_after,  #: int,
+):  # -> Float[ndarray, "time"]:
+    context_mean_before = np.mean(sh[max(start - spike_replace_before, 0) : start])
+    context_mean_after = np.mean(
+        sh[stop + 1 : min(len(sh), stop + spike_replace_after + 1)]
+    )
+
+    sh[start:stop] = (context_mean_before + context_mean_after) / 2
--- turban/test_level2.py
+++ turban/test_level2.py
@@ -2,8 +2,9 @@ import pytest
 import numpy as np
 from .level2 import (
     boolarr_to_sections,
+    sections_to_marker,
     detect_shear_spikes,
-    # replace_spikes,
+    replace_spikes,
     rollpad1,
     enlarge_bool,
 )
@@ -21,19 +22,19 @@ def test_enlarge_bool():
 
 
 def test_boolarr_to_sections():
-    bools = [True, False, False, True, True, False, True]
+    bools = np.array([True, False, False, True, True, False, True])
     result = [[0], [3, 4], [6]]
     assert boolarr_to_sections(bools) == result
 
-    bools = [True, False, False, True, True, False]
+    bools = np.array([True, False, False, True, True, False])
     result = [[0], [3, 4]]
     assert boolarr_to_sections(bools) == result
 
-    bools = [False, False, True, True, False, True]
+    bools = np.array([False, False, True, True, False, True])
     result = [[2, 3], [5]]
     assert boolarr_to_sections(bools) == result
 
-    bools = [False, False, True, True, False]
+    bools = np.array([False, False, True, True, False])
     result = [[2, 3]]
     assert boolarr_to_sections(bools) == result
 
@@ -42,25 +43,26 @@ def test_despike():
     sh = np.ones(100)
     sh[50] = 100
     sh[[49, 51]] = 3
+    sh[80] = 200
+    sh[[79, 81]] = 5
     result = np.ones(100)
-    # sh[[49, 51]] = 1
-
-    sampling_freq = 1024.
-    spike_threshold = 8.
+    sampling_freq = 1024.0
+    spike_threshold = 8.0
     spikes = detect_shear_spikes(
         sh,
         sampling_freq,
         spike_threshold,
         spike_include_before=10,
-        spike_include_after=20,
+        spike_include_after=10,
     )
 
     spike_sections = boolarr_to_sections(spikes)
-    sh_filtered = replace_spikes(
+    spike_markers = sections_to_marker(spike_sections, len(sh))
+    replace_spikes(
         sh,
-        spike_sections,
+        spike_markers,
         spike_replace_before=10,
         spike_replace_after=10,
     )
 
-    assert np.all(result == sh_filtered)
+    assert np.all(result == sh)
