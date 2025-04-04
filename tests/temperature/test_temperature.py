f583d32e9b9a09b0afca4a893e7d9e9d9f5887e6
doppler <ara@akvaplan.niva.no>
2025-04-04 13:41:58 +0200

    wip

--- tests/temperature/test_temperature.py
+++ tests/temperature/test_temperature.py
@@ -1,21 +1,26 @@
-import pytest
+from pytest import fixture
 import numpy as np
-
+from turban.shear.level2 import select_sections
+import xarray as xr
+get_vsink=data=None
 from turban.temperature import * # TODO use named imports
 from turban.temperature.temperature import microtemp
 
-
-def _load_0030():
+@fixture
+def microtemp_mss_data():
+    fname = "/home/doppler/instruments/MSS/data/cast0030.nc"
     ds = xr.open_dataset(
-        "/home/doppler/instruments/MSS/data/cast0030.nc", group="L2_cleaned"
+        fname, group="L2_cleaned"
+
     ).isel(TIME=slice(30000, 200_000))
     pspd = ds.PSPD_REL.values
     secno = ds.SECTION_NUMBER
     ds = xr.open_dataset(
-        "/home/doppler/instruments/MSS/data/cast0030.nc", group="L1_converted"
+        fname, group="L1_converted"
     ).isel(TIME=slice(30000, 200_000))
     dTdt = ds.GRADT.isel(N_GRADT_SENSORS=0).values * pspd
     temp = ds.TEMP.isel(N_TEMP_SENSORS=0).values
+    return dTdt, pspd, secno, temp
 
 
 def _test_temp():
