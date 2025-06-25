26233ae19c98ac6a9740950570010c8bf28fbe27
doppler <ara@akvaplan.niva.no>
2025-06-25 21:22:20 +0200

    move some variables to mss module

--- turban/variables.py
+++ turban/variables.py
@@ -1,15 +1,4 @@
-import json
-import pydantic
 from collections import defaultdict
-
-FIXED_NAMES = {  # TODO into respective instruments module?
-    "TEMP_FAST": "Fast-response temperature",
-    "TEMP_EMPH": "Fast-response temperature, pre-emphasized",
-    "TEMP": "Precision temperature",
-    "Cmdc": "fast response conductivity (low-pass filter to get conductivity)",
-    "Cmac": "fluctuation from fast response conductivity (gradient gives mC gradients)",
-    "COND": "Precision conductivity",
-}
 
 # Physical quantities used in the code base
 _vars = {
