7348e75b6b42aee4b3d5fd776c5a4087eae7f63d
doppler <ara@akvaplan.niva.no>
2025-06-25 21:21:03 +0200

    more variable names

--- turban/variables.py
+++ turban/variables.py
@@ -2,7 +2,7 @@ import json
 import pydantic
 from collections import defaultdict
 
-FIXED_NAMES = { # TODO into respective instruments module?
+FIXED_NAMES = {  # TODO into respective instruments module?
     "TEMP_FAST": "Fast-response temperature",
     "TEMP_EMPH": "Fast-response temperature, pre-emphasized",
     "TEMP": "Precision temperature",
@@ -11,14 +11,9 @@ FIXED_NAMES = { # TODO into respective instruments module?
     "COND": "Precision conductivity",
 }
 
-_generic = {
-    "psi_f/explanation": "Generic power spectrum in units of Hz-1",
-    "psi_k/explanation": "Generic power spectrum in units of cpm-1",
-    "x/explanation": "Generic signal",
-}
-
 # Physical quantities used in the code base
 _vars = {
+    "cond/standard_name": "sea_water_conductivity",
     "eps/atomix_name": "EPSI",
     "eps/latex": r"$\eps$",
     "eps/standard_name": "",
@@ -26,32 +21,45 @@ _vars = {
     "freq/atomix_name": None,
     "freq/explanation": "Number of cycles per second",
     "freq/unit": "Hz",  # cps
-    "psi_k_gradt/explanation": "Power density spectrum of temperature gradient variance w.r.t wavenumber",
-    "psi_k_gradt/long_name": "",
-    "psi_k_gradt/latex": r"\Psi_{Tz}",
-    "psi_k_gradt/unit": "K2 m-2 cpm-1",
-    "psi_k_sh/atomix_name": "SPEC",
-    "psi_k_sh/explanation": "Power density spectrum of shear variance w.r.t wavenumber",
-    "psi_k_sh/long_name": "",
-    "psi_k_sh/unit": "s-2 cpm-1",
+    "kolm_length/explanation": "Kolmogorov length scale",
+    "kolm_length/latex": r"$\eta$",
+    "molvisc/standard_name": "",  # TODO
+    "molvisc/unit": "m2 s-1",
+    "press/explanation": "Sea water pressure",
+    "press/standard_name": "sea_water_pressure",
     "psi_f_gradt/explanation": "Power density spectrum of temperature gradient variance w.r.t frequency",
-    "psi_f_gradt/long_name": "",
     "psi_f_gradt/latex": r"\Psi_{Tz}",
+    "psi_f_gradt/long_name": "",
     "psi_f_gradt/unit": "K2 m-2 Hz-1",
     "psi_f_sh/atomix_name": "SPEC",
     "psi_f_sh/explanation": "Power density spectrum of shear variance w.r.t frequency",
     "psi_f_sh/long_name": "",
     "psi_f_sh/unit": "s-2 Hz-1",
+    "psi_f/explanation": "Generic power spectrum in units of Hz-1",
+    "psi_k_gradt/explanation": "Power density spectrum of temperature gradient variance w.r.t wavenumber",
+    "psi_k_gradt/latex": r"\Psi_{Tz}",
+    "psi_k_gradt/long_name": "",
+    "psi_k_gradt/unit": "K2 m-2 cpm-1",
+    "psi_k_sh/atomix_name": "SPEC",
+    "psi_k_sh/explanation": "Power density spectrum of shear variance w.r.t wavenumber",
+    "psi_k_sh/long_name": "",
+    "psi_k_sh/unit": "s-2 cpm-1",
+    "psi_k/explanation": "Generic power spectrum in units of cpm-1",
+    "sal/standard_name": "sea_water_salinity",  # TODO: absolute?
     "sampling_freq/atomix_name": "fs",
     "sampling_freq/explanation": "Number of samples per second",
     "sampling_freq/unit": "Hz",
+    "senspeed/atomix_name": "PSPD",
+    "senspeed/explanation": "Speed of the sensor relative to water",
     "shear/atomix_name": "SH",
-    "temp/": "",
+    "shear/explanation": "Current shear",
+    "shear/unit": "s-1",
     "temp/standard_name": "sea_water_temperature",
     "temp/unit": "degC",
-    "waveno/atomix_name": "KCYC",
-    "waveno/unit": "cpm",
-    # "waveno/ex": "",
+    "wavno/atomix_name": "KCYC",  # TODO
+    "wavno/explanation": "Number of cycles per metre",
+    "wavno/unit": "cpm",
+    "x/explanation": "Generic signal",
 }
 
 
@@ -64,4 +72,3 @@ def to_dict(vars):
 
 
 VARIABLES = to_dict(_vars)
-GENERIC = to_dict(_generic)
