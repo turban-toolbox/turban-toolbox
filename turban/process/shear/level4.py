52d3c2daebafad6fc65c02366c8f53cf85890fb0
doppler <ara@akvaplan.niva.no>
2025-11-27 10:56:41 +0100

    move to utils

--- turban/process/shear/level4.py
+++ turban/process/shear/level4.py
@@ -150,18 +150,6 @@ def get_quality_metric(
     return quality_metric
 
 
-def unwrap_base2(
-    q: Int[ndarray, "*any"], maxflag: int = 16
-) -> dict[int, Bool[ndarray, "*any"]]:
-    """Unwrap integers into base 2 constituents"""
-    flag_arr: Bool[ndarray, "*any"] = np.unpackbits(
-        q.astype(np.uint8)[np.newaxis], axis=0, bitorder="little"
-    ).astype(bool)
-    base = [2**i for i in range(int(np.log2(maxflag) + 1))]
-    flag_dict = {name: val for name, val in zip(base, flag_arr)}
-    return flag_dict
-
-
 def get_log_diss_var(
     eps_source_flag: Int[ndarray, "nshear time"],
     kolm_length: Float[ndarray, "nshear time"],
--- turban/utils/util.py
+++ turban/utils/util.py
@@ -466,3 +466,17 @@ def define_sections(
         section_number[sec] = k + 1
 
     return section_number
+
+
+def unwrap_base2(
+    q: Int[ndarray, "*any"], maxflag: int = 16
+) -> dict[int, Bool[ndarray, "*any"]]:
+    """Unwrap integers into base 2 constituents"""
+    flag_arr: Bool[ndarray, "*any"] = np.unpackbits(
+        q.astype(np.uint8)[np.newaxis], axis=0, bitorder="little"
+    ).astype(bool)
+    base = [2**i for i in range(int(np.log2(maxflag) + 1))]
+    flag_dict = {name: val for name, val in zip(base, flag_arr)}
+    return flag_dict
+
+
