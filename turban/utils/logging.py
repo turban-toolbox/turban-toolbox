0306e6ccb20362976927b552f0280d436abadfd4
doppler <ara@akvaplan.niva.no>
2025-11-28 10:28:16 +0100

    logger formatting

--- turban/utils/logging.py
+++ turban/utils/logging.py
@@ -2,3 +2,8 @@ import logging
 
 logger = logging.getLogger('turban')
 logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('{asctime} | {levelname:<8s} | {name:<20s} | {message}', style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
