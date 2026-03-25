437038036c38b5ad9c3943eb18d19c9536695600
doppler <ara@akvaplan.niva.no>
2026-03-25 13:53:21 +0100

    extend spectrum function to handle cross-spectra

--- turban/utils/spectra.py
+++ turban/utils/spectra.py
@@ -1,4 +1,6 @@
 from typing import Literal, cast
+from itertools import product
+
 from numpy import ndarray, newaxis
 import numpy as np
 from jaxtyping import Float, Int, Complex, Num
@@ -8,7 +10,7 @@ from turban.utils.util import get_chunking_index
 
 
 def spectrum(
-    x: Float[ndarray, "... time_fast"],
+    x: Float[ndarray, "nx ... time_fast"],
     sampfreq: float,
     segment_length: int,
     segment_overlap: int,
@@ -17,11 +19,12 @@ def spectrum(
     section_number: Int[ndarray, "time_fast"] | None = None,
     reshape_index: Int[ndarray, "diss_chunk chunk_length"] | None = None,
     y: (
-        Float[ndarray, "... time_fast"] | None
+        Float[ndarray, "ny ... time_fast"] | None
     ) = None,  # if not None, return cross spectral density
+    kind: Literal["diagonal", "cross"] = "diagonal",
     **estimator_kwarg,
 ) -> tuple[
-    Num[ndarray, "... chunk freq"],
+    Num[ndarray, "nx ... chunk freq"],
     Float[ndarray, "freq"],  # frequencies
 ]:
     """Compute power spectral density or cross spectral density from time series.
@@ -73,18 +76,40 @@ def spectrum(
         scaling="density",
     )
 
-    xr = x[..., reshape_index]  # reshape to chunk  length windows
+    xr = x[..., reshape_index]  # reshape to chunk length windows
 
-    if y is None:
-        freq, psi = welch(xr, **kwarg, **estimator_kwarg)
-    else:
-        yr = y[..., reshape_index]
-        freq, psi = csd(xr, yr, **kwarg, **estimator_kwarg)
+    match kind:
+        case "diagonal":
+            if y is None:
+                freq, psi = welch(xr, **kwarg, **estimator_kwarg)
+            else:
+                assert (
+                    x.shape[0] == y.shape[0]
+                ), f"For kind {kind}, first dimensions of x and y must match"
+                yr = y[..., reshape_index]
+                freq, psi = csd(xr, yr, **kwarg, **estimator_kwarg)
 
-    dfreq = freq[1] - freq[0]
-    # by using scaling 'density' above, variance should already be approximately conserved,
-    # but now we correct for PSD estimator bias manually:
-    _ = apply_var_conserve(psi, dfreq, xr)
+            dfreq = freq[1] - freq[0]
+            # by using scaling 'density' above, variance should already be approximately conserved,
+            # but now we correct for PSD estimator bias manually:
+            _ = apply_var_conserve(psi, dfreq, xr)
+
+        case "cross":
+            nx = xr.shape[0]
+            comb: Int[ndarray, "n_combinations 2"]
+            if y is None:
+                yr = xr
+                ny = yr.shape[0]
+            else:
+                yr = y[..., reshape_index]  # reshape to chunk length windows
+                ny = y.shape[0]
+
+            comb = np.array(list(product(range(nx), range(ny))))
+            psi: Num[ndarray, "nx*ny ... chunk freq"]
+            freq, psi = csd(xr[comb[:, 0]], yr[comb[:, 1]], **kwarg, **estimator_kwarg)
+            psi: Num[ndarray, "nx ny ... chunk freq"] = psi.reshape(
+                nx, ny, *psi.shape[1:]
+            )
 
     return psi, freq
 
